b25881da89a3d0ed512f497ec747bb881fb255ab
miguel7z <0000111131780xsp@al.educacao.sp.gov.br>
2024-11-05 09:09:37 -0300

    Vai CORINTHIANS

---
+++ perguntas.js
@@ -0,0 +1,22 @@
+criaCartao(
+    'futebol',
+    'qual o maior campeonato de futebol do mundo?',
+    'copa do mundo FIFA',
+)
+criaCartao(
+    'futebol',
+    'Que ano o CORITHIANS ganhou o seu primeiro mundial?',
+    'Em 2000',
+)
+
+criaCartao(
+    'futebol',
+    'Qual e o maior clube de futebol do mundo?',
+    'REAL MADRID',
+)
+
+criaCartao(
+    'futebol',
+    'Que e o maior artilheiro da Chanpions?',
+    'CRISTIANO RONALDO',
+)
